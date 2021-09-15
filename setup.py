2c34e0387e60690d4bd01d61641f808e35e66f35
Claudio Satriano <satriano@gmail.com>
2021-09-15 21:55:39 +0200

    Update PyPI classifiers

--- setup.py
+++ setup.py
@@ -73,10 +73,10 @@ setup(
                 'License, version 2.1 (CeCILL-2.1)',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'Topic :: Scientific/Engineering',
             'Topic :: Scientific/Engineering :: Physics'],
     install_requires=['numpy>=1.18']
