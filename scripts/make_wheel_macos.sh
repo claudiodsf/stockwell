7bcb626c41d6db000895c733e7e097a822e33d38
Claudio Satriano <satriano@gmail.com>
2021-09-15 12:00:34 +0200

    Copy FFTW3 dynamic library into macOS wheel

--- scripts/make_wheel_macos.sh
+++ scripts/make_wheel_macos.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-# Build a wheel on Linux/macOS.
+# Build a wheel on macOS.
 # (c) 2021 Claudio Satriano
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/.. || exit
+rm -rf dist
 python setup.py bdist_wheel || exit
 mkdir -p wheels
-mv dist/*.whl wheels
-cd - || exit
+wheel_name=$(basename dist/*.whl)
+mv dist/$wheel_name wheels
+cd wheels && delocate-wheel $wheel_name
+cd $DIR || exit
