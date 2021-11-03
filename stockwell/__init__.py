fe5e4b6eed785ad44a4101e44c5ee8cfa77a58e1
Claudio Satriano <satriano@gmail.com>
2021-11-03 18:14:32 +0100

    Fix linter warning

--- stockwell/__init__.py
+++ stockwell/__init__.py
@@ -1,5 +1,5 @@
-from .lib import st
-from .lib import sine
+from .lib import st  #NOQA
+from .lib import sine  #NOQA
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
