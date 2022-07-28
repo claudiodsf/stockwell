cdeb2caeeae3444058c174f7232e8550c80e97cf
Claudio Satriano <satriano@gmail.com>
2022-07-28 09:16:11 +0200

    Add Python 3.10 to classifiers in setup.py

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Physics'],
     install_requires=['numpy>=1.18']
