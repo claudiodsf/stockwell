c6a6d0fe17cc25cca9de32c7835d8b0a0900214d
Claudio Satriano <satriano@gmail.com>
2024-05-24 12:05:32 +0200

    Script to run tests on Windows using pytest.
    This script allows for a temporary test failure by repeating the test 5
    times. This is useful to avoid false positives due to random failures.

---
+++ scripts/test_stockwell.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Test Stockwell on Windows using pytest
+REM Allows for a temporary test failure by repeating the test 5 times
+REM (c) 2024 Claudio Satriano <satriano@ipgp.fr>
+set /a n=0
+:loop
+pytest --pyargs stockwell && exit /b 0
+set /a n+=1
+if %n% gtr 5 goto :end
+goto :loop
+:end
