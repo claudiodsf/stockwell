7835ba900d39e511f1caab915639978f35b4a42c
Claudio Satriano <satriano@gmail.com>
2025-01-08 07:50:26 +0100

    Drop support for Python 3.8, add support for Python 3.13

--- setup.py
+++ setup.py
@@ -85,11 +85,11 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Physics'],
     install_requires=['numpy>=1.18']
