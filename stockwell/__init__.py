c221c15288b410e83802c61eadcf1d24ef3c9478
Claudio Satriano <satriano@gmail.com>
2026-01-22 11:28:50 +0100

    Lazy import st and sine modules to avoid requiring compiled libraries at import time.

--- stockwell/__init__.py
+++ stockwell/__init__.py
@@ -8,7 +8,18 @@ Time-frequency analysis through Stockwell transform.
     GNU General Public License v3.0 or later.
     (https://www.gnu.org/licenses/gpl-3.0.html)
 """
-from . import st, sine  # noqa
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+# pylint: disable=import-outside-toplevel
+
+
+# Lazy import of modules to avoid requiring compiled libraries at import time
+def __getattr__(name):
+    if name == 'st':
+        import importlib
+        return importlib.import_module('.st', __name__)
+    if name == 'sine':
+        import importlib
+        return importlib.import_module('.sine', __name__)
+    raise AttributeError(f'module {__name__!r} has no attribute {name!r}')
